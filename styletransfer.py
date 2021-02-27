6918defa066f2075c0e1cbfe5bc0451fa80b5d87
Thom Su <thomsu.nyc@gmail.com>
2021-02-27 14:19:56 -0500

    completed preprocessing

--- styletransfer.py
+++ styletransfer.py
@@ -2,7 +2,7 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 from PIL import Image
-import torchvision.transform as transform
+import torchvision.transforms as transforms
 import torchvision.models as models
 from torchvision.utils import save_image
 
@@ -24,3 +24,32 @@ class VGG(nn.Module):
                 features.append(x)
 
         return features
+
+
+def load_image(image_name):
+    image = Image.open(image_name)
+    image = loader(image).unsqueeze(0)
+    return image.to(device)
+
+
+device = torch.device('cuda' if torch.cuda.is_available else 'cpu')
+image_size = 256
+
+loader = transforms.Compose(
+    [
+        transforms.Resize((image_size, image_size)),
+        transforms.ToTensor(),
+    ]
+)
+
+original_img = load_image('content\ images/ChrisPratt.jpg')
+style_img = load_image('style\ images/StyleKockoshka.jpg')
+model = VGG().to(device).eval()
+generated = original_img.clone().requires_grad_(True)
+
+# Hyperparameters
+total_steps = 6000
+learning_rate = 0.001
+alpha = 1
+beta = 0.01
+optimizer = optim.Adam([generated], lr=learning_rate)
